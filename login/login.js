373abacdc4601b8db50918a26c25316cc2f31538
GUSTAVO AYRES LIMA <47052412829@info.local>
2026-09-04 11:04:41 -0300

    :sparkles: tela de login quase finalizada

---
+++ login/login.js
@@ -0,0 +1,33 @@
+// console.log("hello world")
+
+// alert("hello world")
+
+// var nome = "GUGU"  !NÃO VAMOS USAR
+
+// let nome = " GUGU" !NÃO VAMOS USAR
+
+// const nome = "GUGU";
+
+// nome = "joribe";
+
+// const nome = "GUGU"
+// console.log(nome)
+
+// nome = "joribe"
+// console.log(nome)
+
+const campoSenha = document.getElementById ("senha");
+const btnSenha = document.querySelector("#mostrar-senha");
+
+
+btnSenha.addEventListener("click", function(e) {
+
+    //if(campoSenha.type == "password") {
+    // campoSenha.type = "text";
+    //}
+    //else {
+    // campoSenha.type = "password"
+    //}
+    campoSenha.type = campoSenha.type == "password" ? 
+
+})
